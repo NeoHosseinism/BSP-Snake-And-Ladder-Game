c13d5eea057b28922398b577dc608e7fa8cce993
Huseyn <huseyn.hosseini@gmail.com>
2023-04-03 10:46:36 -0700

    ADD draw the snakes and ladders and board

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 // ignore: unused_import
-import 'dart:math';
+import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 
@@ -28,24 +28,32 @@ class SnakeAndLadderPage extends StatefulWidget {
 }
 
 class _SnakeAndLadderPageState extends State<SnakeAndLadderPage> {
-  // This key will be associated with the amber box
+  late List<List<int>> ladders;
+  late List<List<int>> snakes;
+
+  @override
+  void initState() {
+    final random = math.Random();
+    snakes = List.generate(10, (index) {
+      int startHome = random.nextInt(100);
+      int endHome = random.nextInt(100);
+      return [startHome, endHome];
+    });
+    ladders = List.generate(10, (index) {
+      int startHome = random.nextInt(100);
+      int endHome = random.nextInt(100);
+      return [startHome, endHome];
+    });
+    super.initState();
+  }
+
   final List<GlobalKey> _keys = List.generate(100, (index) => GlobalKey());
 
-  // This function is called when the user presses the floating button
-  void _getOffsetAndSize(GlobalKey key) {
-    RenderBox? box = key.currentContext?.findRenderObject() as RenderBox?;
-    Offset? position = box?.localToGlobal(Offset.zero);
-    if (position != null) {
-      setState(() {
-        final RenderBox renderBox =
-            key.currentContext?.findRenderObject() as RenderBox;
-        key.currentContext?.size;
-
-        final Size size = renderBox.size;
-        debugPrint('Size: ${size.width}, ${size.height}');
-        debugPrint("x: ${position.dx}, y: ${position.dy} ");
-      });
-    }
+  List<double> _getOffsetOfHome(GlobalKey key) {
+    final RenderBox renderBox =
+        key.currentContext!.findRenderObject() as RenderBox;
+    final position = renderBox.localToGlobal(Offset.zero);
+    return [position.dx, position.dy];
   }
 
   @override
@@ -54,7 +62,6 @@ class _SnakeAndLadderPageState extends State<SnakeAndLadderPage> {
       decoration: const BoxDecoration(
         gradient: RadialGradient(
           radius: 0.8,
-
           colors: [
             // Color(0xFFF2A65A),
             // Color(0xFFD2691E),
@@ -65,12 +72,111 @@ class _SnakeAndLadderPageState extends State<SnakeAndLadderPage> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              drawBoard(),
-            ]),
+        body: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                drawBoard(),
+              ],
+            ),
+            FutureBuilder(
+              future: Future.delayed(const Duration(milliseconds: 1)),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Visibility(
+                    visible: true,
+                    child: Center(
+                      child: Container(
+                          padding: const EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.brown[400],
+                          ),
+                          child: const CircularProgressIndicator(
+                              color: Colors.white)),
+                    ),
+                  );
+                } else {
+                  return Visibility(
+                    visible: true,
+                    child: Stack(
+                      children: [
+                        // 92 - 4
+                        CustomPaint(
+                          painter: ArrowPainter(
+                            startX: _getOffsetOfHome(_keys[98])[0],
+                            startY: _getOffsetOfHome(_keys[98])[1],
+                            endX: _getOffsetOfHome(_keys[3])[0],
+                            endY: _getOffsetOfHome(_keys[3])[1],
+                            arrowSize: 22 / 1.75,
+                            color: Colors.red,
+                          ),
+                        ),
+                        // 67 - 6
+                        CustomPaint(
+                          painter: ArrowPainter(
+                            startX: _getOffsetOfHome(_keys[66])[0],
+                            startY: _getOffsetOfHome(_keys[66])[1],
+                            endX: _getOffsetOfHome(_keys[5])[0],
+                            endY: _getOffsetOfHome(_keys[5])[1],
+                            arrowSize: 22 / 1.75,
+                            color: Colors.red,
+                          ),
+                        ),
+                        // 83 - 40
+                        CustomPaint(
+                          painter: ArrowPainter(
+                            startX: _getOffsetOfHome(_keys[82])[0],
+                            startY: _getOffsetOfHome(_keys[82])[1],
+                            endX: _getOffsetOfHome(_keys[30])[0],
+                            endY: _getOffsetOfHome(_keys[30])[1],
+                            arrowSize: 22 / 1.75,
+                            color: Colors.red,
+                          ),
+                        ),
+                        // 23 - 59
+                        CustomPaint(
+                          painter: ArrowPainter(
+                            startX: _getOffsetOfHome(_keys[22])[0],
+                            startY: _getOffsetOfHome(_keys[22])[1],
+                            endX: _getOffsetOfHome(_keys[51])[0],
+                            endY: _getOffsetOfHome(_keys[51])[1],
+                            arrowSize: 22 / 1.75,
+                            color: Colors.green,
+                          ),
+                        ),
+                        // 44 - 94
+                        CustomPaint(
+                          painter: ArrowPainter(
+                            startX: _getOffsetOfHome(_keys[43])[0],
+                            startY: _getOffsetOfHome(_keys[43])[1],
+                            endX: _getOffsetOfHome(_keys[96])[0],
+                            endY: _getOffsetOfHome(_keys[96])[1],
+                            arrowSize: 22 / 1.75,
+                            color: Colors.green,
+                          ),
+                        ),
+                        // 9 - 53
+                        CustomPaint(
+                          painter: ArrowPainter(
+                            startX: _getOffsetOfHome(_keys[8])[0],
+                            startY: _getOffsetOfHome(_keys[8])[1],
+                            endX: _getOffsetOfHome(_keys[57])[0],
+                            endY: _getOffsetOfHome(_keys[57])[1],
+                            arrowSize: 22 / 1.75,
+                            color: Colors.green,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -87,7 +193,7 @@ class _SnakeAndLadderPageState extends State<SnakeAndLadderPage> {
           ),
           itemBuilder: (_, index) {
             return GestureDetector(
-              onTap: () => _getOffsetAndSize(_keys[index]),
+              // onTap: () => _getOffsetAndSize(_keys[index]),
               child: Container(
                 key: _keys[index],
                 decoration: BoxDecoration(
@@ -103,7 +209,9 @@ class _SnakeAndLadderPageState extends State<SnakeAndLadderPage> {
                 ),
                 child: Center(
                   child: Text(
-                    (index + 1).toString(),
+                    ((index ~/ 10) % 2 == 1)
+                        ? (((index ~/ 10) * 20) + 10 - index).toString()
+                        : (index + 1).toString(),
                     style: const TextStyle(color: Colors.amber),
                   ),
                 ),
@@ -131,240 +239,60 @@ class _SnakeAndLadderPageState extends State<SnakeAndLadderPage> {
   }
 }
 
-// class SnakeAndLadderPage extends StatefulWidget {
-//   const SnakeAndLadderPage({super.key});
+// ????????????????????????????????????????????????????????
 
-//   @override
-//   State<SnakeAndLadderPage> createState() => _SnakeAndLadderPageState();
-// }
+class ArrowPainter extends CustomPainter {
+  final Color color;
+  final double strokeWidth;
+  final double arrowSize;
+  double startX;
+  double startY;
+  double endX;
+  double endY;
 
-// class _SnakeAndLadderPageState extends State<SnakeAndLadderPage>
-//     with SingleTickerProviderStateMixin {
-//   static int boardSize = 10;
-//   final int numSnakes = 3;
-//   final int numLadders = 3;
+  ArrowPainter({
+    required this.startX,
+    required this.startY,
+    required this.endX,
+    required this.endY,
+    required this.arrowSize,
+    this.color = Colors.black,
+    this.strokeWidth = 2.5,
+  }) {
+    startX += 22;
+    startY += 22;
+    endX += 22;
+    endY += 22;
+  }
 
-//   // Define the board layout
-//   List<List<int>> board = List.generate(
-//     10,
-//     (i) => List.generate(10, (j) => i * 10 + (i % 2 == 0 ? j : 9 - j)),
-//     growable: false,
-//   );
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = color
+      ..strokeWidth = strokeWidth
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
 
-//   // Define the snake positions
-//   List<int> snakePositions = List.generate(
-//     3,
-//     (i) => Random().nextInt(boardSize * boardSize - 1) + 1,
-//     growable: false,
-//   );
+    Path path = Path();
+    path.moveTo(startX, startY);
+    path.lineTo(endX, endY);
 
-//   // Define the ladder positions
-//   List<int> ladderPositions = List.generate(
-//     3,
-//     (i) => Random().nextInt(boardSize * boardSize - 1) + 1,
-//     growable: false,
-//   );
+    // Draw arrowhead
+    double angle = math.atan2(endY - startY, endX - startX);
+    double arrowX1 = endX - arrowSize * math.cos(angle - math.pi / 6);
+    double arrowY1 = endY - arrowSize * math.sin(angle - math.pi / 6);
+    double arrowX2 = endX - arrowSize * math.cos(angle + math.pi / 6);
+    double arrowY2 = endY - arrowSize * math.sin(angle + math.pi / 6);
+    path.moveTo(endX, endY);
+    path.lineTo(arrowX1, arrowY1);
+    path.moveTo(endX, endY);
+    path.lineTo(arrowX2, arrowY2);
 
-//   // Define the players
-//   int numPlayers = 2;
-//   List<int> playerPositions = List.generate(
-//     2,
-//     (i) => 0,
-//     growable: false,
-//   );
+    canvas.drawPath(path, paint);
+  }
 
-//   // Define the dice roll animation controller
-//   late AnimationController diceController;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     diceController = AnimationController(
-//       vsync: this,
-//       duration: const Duration(milliseconds: 500),
-//     );
-//   }
-
-//   @override
-//   void dispose() {
-//     diceController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Snake and Ladder'),
-//       ),
-//       body: Center(
-//         // ignore: sized_box_for_whitespace
-//         child: Container(
-//           // padding: const EdgeInsets.symmetric(horizontal: 0),
-//           width: 500,
-//           height: 500,
-//           child: Stack(
-//             children: [
-//               // Draw the board
-//               drawBoard(),
-
-//               // Draw the players
-//               for (int i = 0; i < numPlayers; i++) drawPlayers(i),
-
-//               // Draw the dice
-//               drawDice(),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   CustomPaint drawBoard() {
-//     return CustomPaint(
-//       painter: BoardPainter(
-//         boardSize: boardSize,
-//         board: board,
-//         snakePositions: snakePositions,
-//         ladderPositions: ladderPositions,
-//       ),
-//       size: const Size(500, 500),
-//     );
-//   }
-
-//   Positioned drawPlayers(int i) {
-//     return Positioned(
-//       top: (boardSize - playerPositions[i] ~/ boardSize - 1) * 50 + 5,
-//       left: (playerPositions[i] % boardSize) * 50 + 5,
-//       child: Container(
-//         width: 40,
-//         height: 40,
-//         decoration: BoxDecoration(
-//           color: i == 0 ? Colors.red : Colors.green,
-//           shape: BoxShape.circle,
-//         ),
-//       ),
-//     );
-//   }
-
-//   Positioned drawDice() {
-//     return Positioned(
-//       bottom: 50,
-//       left: 50,
-//       child: GestureDetector(
-//         onTap: () {
-//           if (!diceController.isAnimating) {
-//             // Roll the dice
-//             int roll = Random().nextInt(6) + 1;
-//             diceController.forward(from: 0);
-
-//             // Move the player
-//             setState(() {
-//               playerPositions[0] = (playerPositions[0] + roll)
-//                   .clamp(0, boardSize * boardSize - 1);
-//             });
-
-//             // Check for snakes and ladders
-//             int snakeIndex = snakePositions.indexOf(playerPositions[0]);
-//             if (snakeIndex >= 0) {
-//               int tailPosition =
-//                   (snakePositions[snakeIndex] - (snakeIndex + 1) * 10)
-//                       .clamp(0, boardSize * boardSize - 1);
-//               setState(() {
-//                 playerPositions[0] = tailPosition;
-//               });
-//             }
-
-//             int ladderIndex = ladderPositions.indexOf(playerPositions[0]);
-//             if (ladderIndex >= 0) {
-//               int headPosition =
-//                   (ladderPositions[ladderIndex] + (ladderIndex + 1) * 10)
-//                       .clamp(0, boardSize * boardSize - 1);
-//               setState(() {
-//                 playerPositions[0] = headPosition;
-//               });
-//             }
-//           }
-//         },
-//         child: AnimatedBuilder(
-//           animation: diceController,
-//           builder: (context, child) => Transform.rotate(
-//             angle: diceController.value * pi,
-//             child: child,
-//           ),
-//           child: const Icon(
-//             Icons.trip_origin,
-//             size: 100,
-//             color: Colors.grey,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class BoardPainter extends CustomPainter {
-//   final int boardSize;
-//   final List<List<int>> board;
-//   final List<int> snakePositions;
-//   final List<int> ladderPositions;
-
-//   BoardPainter({
-//     required this.boardSize,
-//     required this.board,
-//     required this.snakePositions,
-//     required this.ladderPositions,
-//   });
-
-//   @override
-//   void paint(Canvas canvas, Size size) {
-//     // Define the colors used for drawing
-//     final Paint whitePaint = Paint()..color = Colors.white;
-//     final Paint blackPaint = Paint()..color = Colors.black;
-//     final Paint redPaint = Paint()..color = Colors.red;
-//     final Paint greenPaint = Paint()..color = Colors.green;
-
-//     // Draw the board grid
-//     for (int i = 0; i < boardSize; i++) {
-//       for (int j = 0; j < boardSize; j++) {
-//         canvas.drawRect(
-//           Rect.fromLTWH(j * 50, (boardSize - i - 1) * 50, 50, 50),
-//           whitePaint,
-//         );
-//         canvas.drawRect(
-//           Rect.fromLTWH(j * 50, (boardSize - i - 1) * 50, 50, 50),
-//           blackPaint..style = PaintingStyle.stroke,
-//         );
-//       }
-//     }
-
-//     // Draw the snakes
-//     for (int i = 0; i < snakePositions.length; i++) {
-//       int headRow = boardSize - 1 - snakePositions[i] ~/ boardSize;
-//       int headCol = snakePositions[i] % boardSize;
-//       int tailRow = headRow - i - 1;
-//       int tailCol = headCol + i + 1;
-//       canvas.drawLine(
-//           Offset(headCol * 50 + 25, headRow * 50 + 25),
-//           Offset(tailCol * 50 + 25, tailRow * 50 + 25),
-//           redPaint..strokeWidth = 5);
-//     }
-
-//     // Draw the ladders
-//     for (int i = 0; i < ladderPositions.length; i++) {
-//       int headRow = boardSize - 1 - ladderPositions[i] ~/ boardSize;
-//       int headCol = ladderPositions[i] % boardSize;
-//       int tailRow = headRow - i - 1;
-//       int tailCol = headCol - i - 1;
-//       canvas.drawLine(
-//           Offset(headCol * 50 + 25, headRow * 50 + 25),
-//           Offset(tailCol * 50 + 25, tailRow * 50 + 25),
-//           greenPaint..strokeWidth = 5);
-//     }
-//   }
-
-//   @override
-//   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-//     return true;
-//   }
-// }
+  @override
+  bool shouldRepaint(ArrowPainter oldDelegate) {
+    return false;
+  }
+}
