9d87f1f33c4cdfe3f27dcc1498e7321e68ae1484
Huseyn <huseyn.hosseini@gmail.com>
2023-04-07 00:19:23 -0700

    ADD Decussate tokens move between rows

--- lib/controller/home/home.dart
+++ lib/controller/home/home.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 import '../../global.dart';
@@ -7,27 +8,29 @@ import '../../global.dart';
 class HomeCtrl extends GetxController {
   RxInt currentDiceNum = 6.obs;
   Future<void> showDiceNumAndMovePlayerToken() async {
-    // TODO: check not over flow of 100
     // TODO: 10 => 11
-    // TODO: a player can die another player
 
     currentDiceNum.value = Random().nextInt(6) + 1;
-    // late int destination;
-    // if (players[whoIsTurn.value].homeNo != -1) {
-    //   destination = currentDiceNum.value + players[whoIsTurn.value].homeNo;
-    // } else {
-    //   destination = currentDiceNum.value;
-    // }
 
     for (int i = 0; i < currentDiceNum.value; i++) {
       await Future.delayed(const Duration(milliseconds: 300), () {
         players[whoIsTurn.value].homeNo++;
-        players[whoIsTurn.value].x.value =
-            getOffsetOfHome(keys[players[whoIsTurn.value].homeNo])[0];
-        players[whoIsTurn.value].y.value =
-            getOffsetOfHome(keys[players[whoIsTurn.value].homeNo])[1];
+
+        final int homeNo = players[whoIsTurn.value].homeNo;
+        final GlobalKey homeKey = ((homeNo ~/ 10) % 2 == 1)
+            ? keys[((homeNo ~/ 10) * 20) + 9 - homeNo]
+            : keys[homeNo];
+
+        players[whoIsTurn.value].x.value = getOffsetOfHome(homeKey)[0];
+        players[whoIsTurn.value].y.value = getOffsetOfHome(homeKey)[1];
+        // players[whoIsTurn.value].x.value =
+        //     getOffsetOfHome(keys[players[whoIsTurn.value].homeNo])[0];
+        // players[whoIsTurn.value].y.value =
+        //     getOffsetOfHome(keys[players[whoIsTurn.value].homeNo])[1];
       });
     }
+
+    // TODO: check not over flow of 100
 
     int overLapperIndex = players.indexWhere((element) {
       return element.name != players[whoIsTurn.value].name &&
